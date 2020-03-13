c0422f59b38d2226178dfa0d4e356b97bae49d1c
dheeraj1709 <45442264+dheeraj1709@users.noreply.github.com>
2020-03-13 11:55:58 +0530

    Update timesheetModal.php

--- application/views/timesheetModal.php
+++ application/views/timesheetModal.php
@@ -1,8 +1,39 @@
+	<style type="text/css">
+		.group-span{
+			display:flex;
+			justify-content: space-around;
+			min-width:100%;
+		}
+		.box-time{
+			
+		}
+		.shift-type-select{
+			width:100px;
+		}
+		.buttonn{
+		background-color: #9E9E9E;
+		border: none;
+		color: white;
+		padding: 10px 10px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		margin: 2px
+		}
+		.time-box{
+			cursor:pointer;
+		}
+	</style>
 
+	<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
 <div>
 	<?php 
+	$shift = json_decode($shift);
 		$timesheetDetails = json_decode($timesheetDetails);
-		//print_r($timesheetDetails->timesheet[$y]->employees[$x]->empName);
+		//print_r($timesheetDetails->timesheet[$ya]->employees[$xa]->empName);
 		function timex( $x)
 	{ 
 	    $output;
@@ -32,97 +63,78 @@
 	}
 	return $output;
 }
-		foreach($timesheetDetails->timesheet[$y]->employees[$x]->visits as $visits){
+?>
+<?php 
+if($aT == 'rosteredEmployees'){
+foreach($timesheetDetails->timesheet[$ya]->unrosteredEmployees[$xa]->clockedTimes as $visits){
 	?>
-	<div start-time="<?php echo $visits->signInTime; ?>" end-time="<?php echo $visits->signOutTime; ?>" class="box-time" style="padding:20px">
+	  <div start-time="<?php echo $visits->startTime; ?>" end-time="<?php echo $visits->endTime; ?>" class="box-time" style="padding:20px">
+	  	<span class="group-span">
 		<span><input type="checkbox" name="" checked></span>
-		<span svalue="<?php echo $visits->signInTime; ?>" evalue="<?php echo $visits->signOutTime; ?>" class="time-box"><?php echo timex($visits->signInTime) ."-". timex($visits->signOutTime) ?></span>
+		<span svalue="<?php echo $visits->startTime; ?>" evalue="<?php echo $visits->endTime; ?>" class="time-box"><?php echo timex($visits->startTime) ."-". timex($visits->endTime) ?></span>
 		<span class="new-time-box"></span>
-		<span><?php echo $visits->reason;?></span>
-	<div>
-		<button class="buttonn">Create Pay Roll</button>
-	</div>
+		<span>
+			<select class="shift-type-select" >
+				<?php foreach($shift->payrollTypes as $shift){
+					if($shift->id == "3"){?>
+				<option value="<?php echo $shift->type; ?>" selected factor="<?php echo $shift->factor; ?>"><?php echo $shift->type ?></option>
+			<?php 
+					}
+					else{ ?>
+				<option value="<?php echo $shift->type; ?>" ><?php echo $shift->type ?></option>	
+					<?php 
+					}
+				} ?>
+			</select>
+		</span>
+	</span>
+		<div><?php echo $visits->reason;?></div>
+
 	</div>
 	<?php
 		}
+	}
 	?>
+<?php 
+if($aT == 'unrosteredEmployees'){
+foreach($timesheetDetails->timesheet[$ya]->unrosteredEmployees[$xa]->clockedTimes as $visits){
+	?>
+	  <div start-time="<?php echo $visits->startTime; ?>" end-time="<?php echo $visits->endTime; ?>" class="box-time" style="padding:20px">
+	  	<span class="group-span">
+		<span><input type="checkbox" name="" checked></span>
+		<span svalue="<?php echo $visits->startTime; ?>" evalue="<?php echo $visits->endTime; ?>" class="time-box"><?php echo timex($visits->startTime) ."-". timex($visits->endTime) ?></span>
+		<span class="new-time-box"></span>
+		<span>
+			<select class="shift-type-select" >
+				<?php foreach($shift->payrollTypes as $shift){
+					if($shift->id == "3"){?>
+				<option value="<?php echo $shift->type; ?>" selected factor="<?php echo $shift->factor; ?>"><?php echo $shift->type ?></option>
+			<?php 
+					}
+					else{ ?>
+				<option value="<?php echo $shift->type; ?>" ><?php echo $shift->type ?></option>	
+					<?php 
+					}
+				} ?>
+			</select>
+		</span>
+	</span>
+		<div><?php echo $visits->reason;?></div>
+
+	</div>
+	<?php
+		}
+	}
+	?>
+		
+		<div>
+		<button class="buttonn">Create Pay Roll</button>
+	</div>
 </div>
 
-	<script type="text/javascript">
-	function timer( x)
-	{ 
-	    var output="";
-	    if((x/100) < 12){
-	        if((x%100)==0 ){
-	        	if((x/100)<10){
-	         output = "0"+String(x/100) + ":00" ;
-	     }
-	     if((x/100)>9){
-	     	output = String(x/100) + ":00" ;
-	     }
-	        }
-	    if((x%100)!=0){
-	        if((x/100)<10){
-	         output = "0"+String(x/100) + ":" + String(x%100) ;
-	        }
-	    }
-	     if((x/100)>10){
-	         output = String(x/100) + ":" + String(x%100) ;
-	        }
-	    }
+<script type="text/javascript">
 	
-	else if((x/100)>12){
-	    if((x%100)==0){
-	    output = x/100 + ":00";
-	    }
-	    if((x%100)!=0){
-	    output = x/100 +":" + x%100 ;
-	    }
-	}
-	else{
-	if((x%100)==0){
-	     output = parseInt(x/100) + ":00";
-	    }
-	    if((x%100)!=0){
-	    output = parseInt(x/100) + ":" + x%100;
-	    }
-	}
-	return output;
-}
-
 </script>
-<script type="text/javascript">
-	$(document).on('click','.time-box',function(){
-		var thisValue = $(this).children('.time-box').html();
-		var parentHTML = $('timesheet-form').html();
-		var stime = $(this).attr('start-time');
-		var code = "<input type=\"time\" class=\"sclass\"> - <input type=\"time\" class=\"eclass\">"
-		$(this).empty();
-		$(this).next().html(code)
-		$(this).next().children('.sclass').val(timer($(this).attr('svalue')))
-		$(this).next().children('.eclass').val(timer($(this).attr('evalue')))
-
-		//$(this).html(code)
-		//$(this).children().val(timer(500))
-		//$(this).children('.time-box').html($(this).attr('start-time'))
-	})
-</script>
-<script type="text/javascript">
-	$(document).on('click','.buttonn',function(e){
-		e.preventDefault();
-			$.ajax({
-				
-			})
-		})
-</script>
-	<script
-  src="https://code.jquery.com/jquery-3.4.1.js"
-  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-  crossorigin="anonymous"></script>
-	<script type="text/javascript">
-		$('#stime').val(timer($('.box-time').attr('start-time')))
-	</script>
-
 <!--
 <input type="time" name="stime" id="stime">
 <input type="time" name="etime" id="etime">
