8b567b2843e831b0d50dd03f359de0ee0f7133d3
dheeraj1709 <45442264+dheeraj1709@users.noreply.github.com>
2020-04-19 21:22:56 +0530

    Update Payroll.php

--- api/application/controllers/Payroll.php
+++ api/application/controllers/Payroll.php
@@ -59,6 +59,37 @@ class Payroll extends CI_Controller{
 		}
 	}
 
+		public function updateEntitlement($userid){
+		$headers = $this->input->request_headers();
+		if($headers != null && array_key_exists('x-device-id', $headers) && array_key_exists('x-token', $headers)){
+			$this->load->model('authModel');
+			$res = $this->authModel->getAuthUserId($headers['x-device-id'],$headers['x-token']);
+			$json = json_decode(file_get_contents('php://input'));
+			if($json!= null && $res != null && $res->userid == $json->userid){
+				$id  = $json->id;
+				$name = $json->name;
+				$rate = $json->rate;
+				$userid = $json->userid;
+				$userDetails = $this->authModel->getUserDetails($userid);
+				if($userDetails != null && $userDetails->role == SUPERADMIN){
+					$this->load->model('payrollModel');
+					$this->payrollModel->updateEntitlement($id,$name,$rate);
+					$data['Status'] = 'SUCCESS';
+					http_response_code(200);
+					echo json_encode($data);
+				}
+				else{
+
+					$data['Status'] = 'ERROR';
+					$data['Message'] = "You are not allowed";
+				}
+			}
+		}
+		else{
+			http_response_code(401);
+		}
+	}
+
 	public function addEntitlement(){
 		$headers = $this->input->request_headers();
 		if($headers != null && array_key_exists('x-device-id', $headers) && array_key_exists('x-token', $headers)){
@@ -124,4 +155,66 @@ class Payroll extends CI_Controller{
 			http_response_code(401);
 		}
 	}
+
+	public function deleteEntitlement($entitlementId,$userid){
+		$headers = $this->input->request_headers();
+		   if($headers != null && array_key_exists('x-device-id', $headers) && array_key_exists('x-token', $headers)){
+			$this->load->model('authModel');
+			$res = $this->authModel->getAuthUserId($headers['x-device-id'],$headers['x-token']);
+			if($res != null && $res->userid == $userid){
+				$this->load->model('payrollModel');
+					$this->payrollModel->deleteEntitlement($entitlementId);
+					$data['status'] = 'SUCCESS';
+				}
+				http_response_code(200);
+				echo json_encode($data);
+			}
+			else{
+				http_response_code(401);
+				echo 'ERROR';
+			}
+	}
+
+	public function getUserLevels($x,$userid ){
+		$headers = $this->input->request_headers();
+		if($headers != null && array_key_exists('x-device-id', $headers) && array_key_exists('x-token', $headers)){
+			$this->load->model('authModel');
+			$res = $this->authModel->getAuthUserId($headers['x-device-id'],$headers['x-token']);
+			if($res != null && $res->userid == $userid){
+				$this->load->model('payrollModel');
+				$mdata['users'] = $this->payrollModel->getUserLevels($x);
+				http_response_code(200);
+				echo json_encode($mdata);
+			}
+			else{
+				http_response_code(401);
+			}
+		}
+		else{
+			http_response_code(401);
+		}
+	}
+
+	// public function updateEntitlement($entitlementId){
+	// 	$headers = $this->input->request_headers();
+	// 	   if($headers != null && array_key_exists('x-device-id', $headers) && array_key_exists('x-token', $headers)){
+	// 		$this->load->model('authModel');
+	// 		$res = $this->authModel->getAuthUserId($headers['x-device-id'],$headers['x-token']);
+	// 		$json = json_decode(file_get_contents('php://input'));
+	// 		if($res != null && $res->userid == $json->suserid){
+	// 			$name = $json->name;
+	// 			$id
+	// 			$hourlyRate = $json->hourlyRate;
+	// 			$this->load->model('payrollModel');
+	// 				$this->payrollModel->updateEntitlement($entitlementId,$name,$hourlyRate);
+	// 				$data['status'] = 'SUCCESS';
+	// 			}
+	// 			http_response_code(200);
+	// 			echo json_encode($data);
+	// 		}
+	// 		else{
+	// 			http_response_code(401);
+	// 			echo 'ERROR';
+	// 		}
+	// 	}
 }
