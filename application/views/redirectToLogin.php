ac60bbf373551295f04bccb4cfd2aba0ecf4c2f6
dheeraj1709 <45442264+dheeraj1709@users.noreply.github.com>
2020-05-01 18:53:23 +0530

    Add files via upload

---
+++ application/views/redirectToLogin.php
@@ -0,0 +1,20 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<style type="text/css">
+	.page{
+	 display: flex;
+	 justify-content:center;
+
+	 overflow-y:hidden;
+	}
+	.pag{
+	  margin:40vh auto;
+	  align-content:center;
+	}
+</style>
+<div class="page">
+  <div class="pag">
+  <div class=""><h1>You are logged out !!</h1></div>
+  <div class=""><a href="<?php echo base_url().'welcome/login'?>">Click here to login</a></div>
+  </div>
+</div>
