1067e6e93477ee44ae9c52dea7e8683405368490
dheeraj1709 <45442264+dheeraj1709@users.noreply.github.com>
2020-05-10 08:56:01 +0530

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -5,7 +5,11 @@
 	  <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css');?>">
- <style>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+<style type="text/css">
+    *{
+font-family: 'Open Sans', sans-serif;
+    }
  body{
 	background-color:#307bd3;
 }
