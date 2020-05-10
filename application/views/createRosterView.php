1271c365d7a6ad0b568f31a43e3c7f335d73e116
dheeraj1709 <45442264+dheeraj1709@users.noreply.github.com>
2020-05-10 08:43:35 +0530

    Update createRosterView.php

--- application/views/createRosterView.php
+++ application/views/createRosterView.php
@@ -8,7 +8,11 @@
 	<link href="https//:maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="https//:maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="https//:cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 <style type="text/css">
+	*{
+font-family: 'Open Sans', sans-serif;
+	}
 .containers{
 	width:100%;
 }
