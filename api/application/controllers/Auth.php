683eeedea2c44d8d17d55107abfaa531535778fd
dheeraj1709 <dheeraj1709@github.com>
2021-06-10 14:09:31 +0530

    Update Auth.php

--- api/application/controllers/Auth.php
+++ api/application/controllers/Auth.php
@@ -51,7 +51,7 @@ class Auth extends CI_Controller
 					$data['role'] = $user->role;
 					$data['title'] = $user->title;
 					$data['manager'] = $user->manager;
-					$data['companyImage'] = $companyDetails->companyLogo;
+					$data['companyImage'] = isset($companyDetails->companyLogo) ? $companyDetails->companyLogo : null;
 					//$data['firebaseid'] = $user->firebaseid;
 					$data['isVerified'] = $user->isVerified;
 					$permissions = $this->authModel->getPermissions($user->id);
