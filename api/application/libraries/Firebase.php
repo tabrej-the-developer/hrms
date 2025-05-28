9d58b00f5642f2c984d4685aec0f63074f249463
Tabrej Khan <tabrezk294@gmail.com>
2025-05-28 17:44:05 +0530

    firebase comment

--- api/application/libraries/Firebase.php
+++ api/application/libraries/Firebase.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-require __DIR__.'/../../vendor/autoload.php';
+// require __DIR__.'/../../vendor/autoload.php';
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
 use Kreait\Firebase\Messaging\CloudMessage;
